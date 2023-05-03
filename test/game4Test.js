e9c602094f80ab2503735d5d0c19cf30783255fb
Thomas Erhel <thomas.erhel@gmail.com>
2023-05-03 15:38:25 +0200

    ✅✅✅✅✅

--- test/game4Test.js
+++ test/game4Test.js
@@ -6,14 +6,25 @@ describe('Game4', function () {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
 
-    return { game };
+    const signer1 = ethers.provider.getSigner(0);
+    const signer2 = ethers.provider.getSigner(1);
+
+    return { game, signer1, signer2 };
   }
+
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer1, signer2 } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
 
-    await game.win();
+    const addr1 = await signer1.getAddress();
+    const addr2 = await signer2.getAddress();
+
+    // Write to the nested mapping using signer2
+    await game.connect(signer2).write(addr1);
+
+    // Call win() function using signer1
+    await game.connect(signer1).win(addr2);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
--- test/game5Test.js
+++ test/game5Test.js
@@ -1,5 +1,6 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { utils } = require('ethers');
 
 describe('Game5', function () {
   async function deployContractAndSetVariables() {
@@ -8,12 +9,38 @@ describe('Game5', function () {
 
     return { game };
   }
+
   it('should be a winner', async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
 
-    await game.win();
+    // Find a private key that generates an address lower than the threshold
+    let privateKey;
+    let wallet;
+    let threshold = '0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf';
+
+    let i = 1;
+    while (true) {
+      privateKey = '0x' + i.toString(16).padStart(64, '0');
+      wallet = new ethers.Wallet(privateKey, ethers.provider);
+      if (ethers.utils.hexlify(wallet.address) < threshold) {
+        break;
+      }
+      i++;
+    }
+
+    // Get a default Hardhat account to transfer Ether
+    const defaultAccount = ethers.provider.getSigner(0);
+
+    // Transfer Ether to the new wallet
+    await defaultAccount.sendTransaction({
+      to: wallet.address,
+      value: utils.parseEther('1')
+    });
+
+    // Call win() function using the new wallet
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
